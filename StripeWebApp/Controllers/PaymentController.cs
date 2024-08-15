a390a8fae16be6b8f55f8444b344919c5bf93840
Isaac Peña <isaacpm01@outlook.com>
2024-08-14 20:03:04 -0400

    Add project files.

---
+++ StripeWebApp/Controllers/PaymentController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stripe.Checkout;
+using Stripe.Terminal;
+using StripeWebApp.Data;
+using StripeWebApp.Models;
+
+namespace StripeWebApp.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly MvcContext _context;
+
+        public PaymentController(MvcContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Items.ToListAsync());
+        }
+      
+
+        [HttpPost]
+        public ActionResult CreateCheckout([Bind("Id, Name, ImageUrl, PriceId")] Item item)
+        {
+            var domain = "https://localhost:7115";
+            var options = new SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>
+                {
+                  new SessionLineItemOptions
+                  {
+                    // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                    Price = $"{item.PriceId}",
+                    Quantity = 1,
+                  },
+                },
+                Mode = "payment",
+                SuccessUrl = domain + "/success.html",
+                CancelUrl = domain + "/cancel.html",
+            };
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+        }
+
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+
+        public IActionResult Cancel()
+        {
+            return View();
+        }
+
+    }
+}
